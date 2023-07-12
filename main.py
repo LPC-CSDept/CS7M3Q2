56d0166c8d20e2767bb2637fef8e7c116d98f212
Kyu Lee <kwooler@gmail.com>
2023-07-11 22:11:29 -0700

    M3Q2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def split(numbers):
+import random
+
+
+def getSum(numbers, n):
     """
     ########################################
     Code Your Program here
@@ -8,15 +11,28 @@ def split(numbers):
 
 def main():
     numbers = [3, 8, 9, 6, 5]
-    ret = split(numbers)
+    print(numbers)
+    ret = getSum(numbers, 3)
     print(f'Your return value: {ret}')
 
     numbers = [1, 2, 3, 4, 5]
-    ret = split(numbers)
+    print(numbers)
+    ret = getSum(numbers, 3)
     print(f'Your return value: {ret}')
 
     numbers = [5, 4, 3, 2, 1]
-    ret = split(numbers)
+    print(numbers)
+    ret = getSum(numbers, 5)
+    print(f'Your return value: {ret}')
+
+    numbers = [5, 4, 3, 2, 1]
+    print(numbers)
+    ret = getSum(numbers, 1)
+    print(f'Your return value: {ret}')
+
+    numbers = [random.randint(-50, 50) for _ in range(10)]
+    print(numbers)
+    ret = getSum(numbers, 5)
     print(f'Your return value: {ret}')
 
 
--- main_test.py
+++ main_test.py
@@ -1,53 +1,44 @@
 import main
-import io
-import sys
-import re
-import math
+import random
 
 
 def test_main_1():
-    captureout = io.StringIO()
-    sys.stdout = captureout
-    # datastr = 'Python\n3 4 1 2 0 5'
-    # sys.stdin = io.StringIO(datastr)
-
-    sys.stdout = sys.__stdout__
-    print('captured ', captureout.getvalue())
-    lines = captureout.getvalue().split('\n')
-    print(lines)
-
     numbers = [3, 8, 9, 6, 5]
-    pivot = numbers[-1]
-    ret = main.split(numbers)
+    ret = main.getSum(numbers, 3)
     print(f'Your return value: {ret}')
-    assert ret[1] == 5, 'It must be pivot'
-    assert True == all(x for x in numbers[:1] if x < pivot)
-    assert True == all(x for x in numbers[2:] if x > pivot)
-    # regex_string = r'[\w,\w]*1'
-    # regex_string += r'[\w,\w]*3'
-    # regex_string += r'[\w,\w]*5'
-    # regex_string += r'[\w,\w]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != none
-    # print(res.group())
+    assert ret == 23
 
 
 def test_main_2():
     numbers = [1, 2, 3, 4, 5]
-    pivot = numbers[-1]
-    ret = main.split(numbers)
+    ret = main.getSum(numbers, 3)
     print(f'Your return value: {ret}')
-    assert ret[4] == 5, 'It must be pivot'
-    assert True == all(x for x in numbers[:4] if x < pivot)
-    assert True == all(x for x in numbers[4:] if x > pivot)
+    assert ret == 12
 
 
 def test_main_3():
     numbers = [5, 4, 3, 2, 1]
-    pivot = numbers[-1]
-    ret = main.split(numbers)
+    ret = main.getSum(numbers, 5)
     print(f'Your return value: {ret}')
-    assert ret[0] == 1, 'It must be pivot'
-    assert True == all(x for x in numbers[:0] if x < pivot)
-    assert True == all(x for x in numbers[0:] if x > pivot)
+    assert ret == 15
+
+
+def test_main_4():
+    numbers = [5, 4, 3, 2, 1]
+    ret = main.getSum(numbers, 1)
+    print(f'Your return value: {ret}')
+    assert ret == 5
+
+
+def test_main_5():
+    numbers = [19, 5, -40, 44, 23, 33, -30, 33, -50, -25]
+    ret = main.getSum(numbers, 5)
+    print(f'Your return value: {ret}')
+    assert ret == 103
+
+
+def test_main_6():
+    numbers = [-2, -3, -47, 50, -40, -29, -38, 16, -13, -35]
+    ret = main.getSum(numbers, 5)
+    print(f'Your return value: {ret}')
+    assert ret == -41
